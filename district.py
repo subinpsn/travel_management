487f6cbc7790c9c4e8cefbdd9562dd63977f8c5f
subin_psn <62749111+subinpsn@users.noreply.github.com>
2021-12-12 14:50:39 +0530

    updated district.py
    added more district distance details

--- district.py
+++ district.py
@@ -1,7 +1,183 @@
 dist_details = [
     {
-    'From': 'Trichy',
-    'To': 'kanyakumari',
-    'Distance': 385
+        'From': 'trichy',
+        'To': 'kanyakumari',
+        'Distance': 385
+    },
+    {
+        'From': 'trichy',
+        'To': 'Chennai',
+        'Distance': 330
+    },
+    {
+        'From': 'trichy',
+        'To': 'salem',
+        'Distance': 140
+    },
+    {
+        'From': 'trichy',
+        'To': 'erode',
+        'Distance': 153
+    },
+    {
+        'From': 'trichy',
+        'To': 'coimbatore',
+        'Distance': 217
+    },
+    {
+        'From': 'trichy',
+        'To': 'thanjavur',
+        'Distance': 57
+    },
+    {
+        'From': 'trichy',
+        'To': 'vellore',
+        'Distance': 268
+    },
+    {
+        'From': 'trichy',
+        'To': 'nagappattinam',
+        'Distance': 147
+    },
+    {
+        'From': 'trichy',
+        'To': 'kanchipuram',
+        'Distance': 282
+    },
+    {
+        'From': 'trichy',
+        'To': 'madurai',
+        'Distance': 137
+    },
+    {
+        'From': 'trichy',
+        'To': 'cuddalore',
+        'Distance': 186
+    },
+    {
+        'From': 'trichy',
+        'To': 'dharmapuri',
+        'Distance': 282
+    },
+    {
+        'From': 'trichy',
+        'To': 'namakkal',
+        'Distance': 88
+    },
+    {
+        'From': 'trichy',
+        'To': 'karur',
+        'Distance': 82
+    },
+    {
+        'From': 'trichy',
+        'To': 'tirunelveli',
+        'Distance': 296
+    },
+    {
+        'From': 'trichy',
+        'To': 'tiruvannamalai',
+        'Distance': 223
+    },
+    {
+        'From': 'trichy',
+        'To': 'dindugal',
+        'Distance': 100
+    },
+    {
+        'From': 'trichy',
+        'To': 'ramanathapuram',
+        'Distance': 182
+    },
+    {
+        'From': 'trichy',
+        'To': 'krishagiri',
+        'Distance': 253
+    },
+    {
+        'From': 'trichy',
+        'To': 'ariyalur',
+        'Distance': 85
+    },
+    {
+        'From': 'trichy',
+        'To': 'tiruppur',
+        'Distance': 174
+    },
+    {
+        'From': 'trichy',
+        'To': 'sivagangai',
+        'Distance': 100
+    },
+    {
+        'From': 'trichy',
+        'To': 'perambalur',
+        'Distance': 56
+    },
+    {
+        'From': 'trichy',
+        'To': 'kallakurichi',
+        'Distance': 147
+    },
+    {
+        'From': 'trichy',
+        'To': 'mayiladuthurai',
+        'Distance': 121
+    },
+    {
+        'From': 'trichy',
+        'To': 'nilgiris',
+        'Distance': 300
+    },
+    {
+        'From': 'trichy',
+        'To': 'pudukkottai',
+        'Distance': 58
+    },
+    {
+        'From': 'trichy',
+        'To': 'ranipet',
+        'Distance': 287
+    },
+    {
+        'From': 'trichy',
+        'To': 'tenkasi',
+        'Distance': 299
+    },
+    {
+        'From': 'trichy',
+        'To': 'theni',
+        'Distance': 184
+    },
+    {
+        'From': 'trichy',
+        'To': 'thothukudi',
+        'Distance': 283
+    },
+    {
+        'From': 'trichy',
+        'To': 'tirpattur',
+        'Distance': 267
+    },
+    {
+        'From': 'trichy',
+        'To': 'tiruvarur',
+        'Distance': 115
+    },
+    {
+        'From': 'trichy',
+        'To': 'villupuram',
+        'Distance': 163
+    },
+    {
+        'From': 'trichy',
+        'To': 'virudhunagar',
+        'Distance': 189
+    },
+    {
+        'From': 'trichy',
+        'To': 'chengalpattu',
+        'Distance': 269
     }
+
 ]
